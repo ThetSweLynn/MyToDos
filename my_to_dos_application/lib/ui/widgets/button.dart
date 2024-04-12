0f38e9b21bdf7c9ca8f069a99429b76ad1bc73e3
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2024-04-12 15:45:29 +0700

    ....

--- my_to_dos_application/lib/ui/widgets/button.dart
+++ my_to_dos_application/lib/ui/widgets/button.dart
@@ -14,7 +14,7 @@ class MyButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 200,
+        width: 185,
         height: 60,
         padding: EdgeInsets.all(17),
         decoration: BoxDecoration(
